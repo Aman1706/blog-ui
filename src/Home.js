f88e089a628c21b18f4350dd030837ed18d760f4
Aman N <aman.narayan1998@gmail.com>
2019-10-29 14:43:49 +0530

    Made Changes to how posts are displayed

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+
+import {Row, Col} from 'reactstrap'
+import {
+    Card, CardImg, CardBody,
+    CardTitle
+  } from 'reactstrap'
 
 function Home(props){
         return (
             <div>
                 <h1 className="text-light bg-dark p-3">Recent Posts - 10</h1>
-                 <ListGroup>
+                 <Row>
                     {props.posts.reverse().slice(0,10).map(post=>{
-                    return <ListGroupItem action key={post.id}><Link to={`/posts/${post.id}`}>{post.title}</Link></ListGroupItem>
-                    }
-                     )}
-                </ListGroup> 
+                    return (
+                        <Col md="3" className="mb-3" key={post.id}>
+                        <Card>
+                        <CardImg top width="100%" src="https://www.w3schools.com/w3css/img_lights.jpg" alt="Card image cap" />
+                        <CardBody>
+                        <CardTitle>{post.title}</CardTitle>
+                        <CardSubtitle>{post.body}</CardSubtitle>
+                        <CardText><Link to={`posts/${post.id}`}>Read More</Link></CardText>
+                        </CardBody>
+                        </Card>
+                        </Col>
+                    // )<ListGroupItem action key={post.id}><Link to={`/posts/${post.id}`}>{post.title}</Link></ListGroupItem>
+                    )
+                    })}
+                </Row> 
             </div>
 
         )
